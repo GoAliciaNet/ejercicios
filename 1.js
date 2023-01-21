1d1ed17ceadceb5772ef391b10e2036c8ef47ded
Alicia Flores <alicia.flores@gonet.us>
2023-01-21 10:12:22 -0600

    First exercise solved

--- 1.js
+++ 1.js
@@ -3,8 +3,26 @@
 // crea una función findUniq que retorne el elemento único.
 
 function findUniq(array) {
+
+    const quantity = array.reduce((counter, item) => {
+        counter[item] = (counter[item] || 0) + 1;
+        return counter
+    }, {});
+
+    console.log('quantity', quantity)
+    let unique = []
+
+
+
+    for (let key in quantity){
+        if(quantity[key] === 1){
+            unique.push(key);
+        }
+      }
+      return unique
 }
 
+console.log(findUniq(['12', 10, '12', 11, 1, 11, 10, '12']))
 
 /**
  * TEST Ejercicio 1
